56ecff8969b48e49646419d776af4911860dae35
steliostss <tsagarakis.stelios@gmail.com>
2019-07-19 16:23:40 +0300

    Small changes

--- MPI/src/MPI_bcast.cpp
+++ MPI/src/MPI_bcast.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include "/usr/include/mpi/mpi.h"
 
+#define MESSAGES_TO_SENT 20
+
 int main (int argc, char** argv) {
 
     // Initialize the MPI environment
@@ -13,11 +15,35 @@ int main (int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // Get the rank of the process
+    // Each process gets its own rank
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     
+    // Count time for each process
     double start = MPI_Wtime();
-    int number;
+    // int number_to_send;
+
+    int message_counter = 0;
+    while (message_counter < MESSAGES_TO_SENT) {
+        if (world_rank == 0) {        // only root will broadcast
+        // ------------------------------------------------------------------
+        // TODO: make it so every process is able to send
+        // ------------------------------------------------------------------
+            for (int i=1; i<world_size; ++i) {
+                MPI_Send(
+                    &message_counter, /* Buffer from which to get data  */
+                    1,                /* Count of data                  */
+                    MPI_INT,          /* MPI_type of data               */
+                    i,                /* Destination process            */
+                    i,                /* Tag                            */
+                    MPI_COMM_WORLD    /* Communicator                   */
+                );
+            }
+        }
+        else if ( get_world_rank == world_rank) {
+
+        }
+    }
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
--- MPI/src/MPI_ping_pong.cpp
+++ MPI/src/MPI_ping_pong.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <unistd.h>
 #include "/usr/include/mpi/mpi.h"
 
 #define PING_PONG_LIMIT 20
