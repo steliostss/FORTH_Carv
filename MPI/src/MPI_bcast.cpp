03f0497b2d86cd9594ae9bd3a9467af7bdb099d6
steliostss <tsagarakis.stelios@gmail.com>
2019-07-19 15:29:40 +0300

    Delete unused lines

--- MPI/src/MPI_bcast.cpp
+++ MPI/src/MPI_bcast.cpp
@@ -18,51 +18,12 @@ int main (int argc, char** argv) {
     
     double start = MPI_Wtime();
     int number;
-    if (world_rank == 0)
-    {
-        // If we are rank 0, set the number to -1 and send it to process 1
-        number = -1;
-        MPI_Send(
-            /* data         = */ &number,
-            /* count        = */ 1,
-            /* datatype     = */ MPI_INT,
-            /* destination  = */ 1,
-            /* tag          = */ 0,
-            /* communicator = */ MPI_COMM_WORLD);
-    }
-    else if (world_rank == 1)
-    {
-        MPI_Recv(
-            /* data         = */ &number,
-            /* count        = */ 1,
-            /* datatype     = */ MPI_INT,
-            /* source       = */ 0,
-            /* tag          = */ 0,
-            /* communicator = */ MPI_COMM_WORLD,
-            /* status       = */ MPI_STATUS_IGNORE);
-        printf("Process 1 received number %d from process 0\n", number);
-    }
-
-  //SEND Section
-    for (int i=0; i<world_size-1; ++i) {
-        MPI_Send(&i, 1, MPI_INT, i, i+100, MPI_COMM_WORLD);
-        std::cout << "Integer sent" << std::endl;
-        // MPI_Send(&send_message, 2, MPI_INT, i, i+200, MPI_COMM_WORLD);
-        // std::cout << "Class sent" << std::endl;
-    }
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
 
     MPI_Get_processor_name(processor_name, &name_len);
-
-   int recv_int;
-    //RECV Section
-    for (int i=0; i<world_size-1; ++i) {
-        MPI_Recv( &recv_int, 1, MPI_INT, i, i+100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        // MPI_Recv( &recv_message, 2 , _____ , i+200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
 
     double end = MPI_Wtime();
    
