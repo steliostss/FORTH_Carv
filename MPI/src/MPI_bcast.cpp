4fe848e593e657b2b3fa141a5f4d5259ada13b36
steliostss <tsagarakis.stelios@gmail.com>
2019-07-22 13:56:53 +0300

    Broadcast works

--- MPI/src/MPI_bcast.cpp
+++ MPI/src/MPI_bcast.cpp
@@ -2,66 +2,38 @@
 #include <unistd.h>
 #include "/usr/include/mpi/mpi.h"
 
-#define MESSAGES_TO_SENT 20
+#define MESSAGES_TO_SENT 2
 
 int main (int argc, char** argv) {
 
-    // Initialize the MPI environment
-    // MPI_Init(&argc, &argv);
-    MPI_Init(NULL, NULL);
+    MPI::Init(argc, argv);
+    int world_size = MPI::COMM_WORLD.Get_size();
+    int world_rank = MPI::COMM_WORLD.Get_rank();
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int data(0);
+    int count(0);
+    int message_counter(0);
 
-    // Get the rank of the process
-    // Each process gets its own rank
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    
-    // Count time for each process
-    double start = MPI_Wtime();
-    // int number_to_send;
-
-    int message_counter = 0;
-    while (message_counter < MESSAGES_TO_SENT) {
-        if (world_rank == 0) {        // only root will broadcast
-        // ------------------------------------------------------------------
-        // TODO: make it so every process is able to send
-        // ------------------------------------------------------------------
-            for (int i=1; i<world_size; ++i) {
-                MPI_Send(
-                    &message_counter, /* Buffer from which to get data  */
-                    1,                /* Count of data                  */
-                    MPI_INT,          /* MPI_type of data               */
-                    i,                /* Destination process            */
-                    i,                /* Tag                            */
-                    MPI_COMM_WORLD    /* Communicator                   */
-                );
+    while (message_counter++ < MESSAGES_TO_SENT) {
+        if (world_rank == 0) {
+            // If we are the root process, send our data to everyone
+            for (int i = 1; i < world_size; i++) {
+                if (i != world_rank) {
+                    MPI::COMM_WORLD.Isend(&data, count, MPI_INT, i, i);
+                    std::cout << "Root: " << world_rank << " just sent a message to: ";
+                    std::cout << i << ", with tag: " << i << "..." << std::endl;
+                }
             }
         }
-        else if ( get_world_rank == world_rank) {
-
+        else {
+            for (int i=1; i< world_size; i++) {
+                // If we are a receiver process, receive the data from the root
+                MPI::COMM_WORLD.Irecv(&data, count, MPI_INT, 0, i);
+                std::cout << "Process: " << world_rank << " just received a message from root...";
+                std::cout << ", with tag: " << i << "..." << std::endl;
+            }
         }
     }
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    double end = MPI_Wtime();
-   
-    usleep(60);
-    // Print off a hello world message
-    std::cout << "Hello world from processor " << processor_name;
-    std::cout << ", rank " << world_rank;
-    std::cout << " out of " << world_size << std::endl;
-    std::cout << "The process took " << end - start << " seconds to run." << std::endl;
-    fflush(stdout);
-
-   // Finalize the MPI environment.
-    MPI_Finalize();
+    MPI::Finalize();
     return 0;
 }
