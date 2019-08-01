68cde7b4e0098e6046d854244e9751f785ac4156
steliostss <tsagarakis.stelios@gmail.com>
2019-08-01 16:33:27 +0300

    Scatter works, with simple scatter algorithm

--- MPI/src/MPI_scatter.cpp
+++ MPI/src/MPI_scatter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <ostream>
 #include <string>
 #include <vector>
 #include <math.h>
@@ -23,12 +24,11 @@ typedef std::tuple<int, int> int_Tup;
  ********* DECLARATIONS OF FUNCTIONS *************
  *************************************************/
 
-void fill_array(sPtr_int array, int array_size);
+void fill_array(int *array, int array_size);
 uPtr_intVec string_to_vec(std::string myStr);
 uPtr_intVec handle_command_line_args(int argc, char **argv, int *array_size, int *sender);
 int calculate_partition_size_for_proc(sPtr_intVec recv, int world_rank, int array_size, int sender);
-
-void alt_MPI_Scatter(sPtr_int sendbuf,       // address of send buffer
+void alt_MPI_Scatter(int* sendbuf,       // address of send buffer
                      int array_size,         // size of buffer
                      sPtr_intVec recv,       // recipients
                      MPI::Datatype sendtype, // type of data in send buffer
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
   int sender = -1;
   sPtr_intVec recv = handle_command_line_args(argc, argv, &array_size, &sender);
 
-  sPtr_int Ssend_array(new int[array_size]);
+  int* Ssend_array(new int[array_size]);
   fill_array(Ssend_array, array_size);
 
   alt_MPI_Scatter(Ssend_array,      // send buffer
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
  ********* IMPLEMENTATIONS OF FUNCTIONS **********
  *************************************************/
 
-void alt_MPI_Scatter(sPtr_int sendbuf,       // address of send buffer
+void alt_MPI_Scatter(int* sendbuf,       // address of send buffer
                      int array_size,         // size of buffer
                      sPtr_intVec recv,       // recipients
                      MPI::Datatype sendtype, // type of data in send buffer
@@ -90,21 +90,24 @@ void alt_MPI_Scatter(sPtr_int sendbuf,       // address of send buffer
   int offset_of_partition(0);
 
   if (world_rank == sender)
-  { //send to all
+  { // send to all
     while (counter++ < recipients)
-    {
+    { // for all recipients
 
       int elements = calculate_partition_size_for_proc(recv, *next_recip, array_size, sender);
       int *partitioned_table = new int[elements];
-      if (next_recip != recv->end())
-        next_recip++;
+      // std::cout << "Sending: ";
       for (int i = 0; i < elements; ++i)
       { // create partitioned table from original
         partitioned_table[i] = sendbuf[offset_of_partition + i];
+        // std::cout << partitioned_table[i] << " ";
       }
       offset_of_partition += elements;
-      MPI::COMM_WORLD.Isend(partitioned_table, elements, MPI::INT, *next_recip, 0);
-      // std::cout << "Sender: " << world_rank << " just sent to: " << next_recip << std::endl;
+      
+      MPI::COMM_WORLD.Isend(partitioned_table, elements, sendtype, *next_recip, 0);
+      // std::cout << "Sender: " << world_rank << " just sent to: " << *next_recip << std::endl;
+      if (next_recip != recv->end())
+        next_recip++;
     }
   }
 
@@ -114,15 +117,18 @@ void alt_MPI_Scatter(sPtr_int sendbuf,       // address of send buffer
     int world_rank = MPI::COMM_WORLD.Get_rank();
     int elements = calculate_partition_size_for_proc(recv, world_rank, array_size, sender);
     int *recvbuf = new int[elements];
-    MPI_Request request = MPI::COMM_WORLD.Irecv(recvbuf, elements, MPI::INT, sender, 0);
-    // MPI_Status stat;
+    MPI_Request request = MPI::COMM_WORLD.Irecv(recvbuf, elements, recvtype, sender, 0);
     MPI_Wait(&request, MPI_STATUS_IGNORE);
+    std::cout << "Proc: " << world_rank << " just recieved: ";
+    for (int i=0; i<elements; ++i) 
+      std::cout << recvbuf[i] << ' ';
+    std::cout << std::endl;
   }
 
   return;
 }
 
-void fill_array(sPtr_int array, int array_size)
+void fill_array(int* array, int array_size)
 {
   for (int i = 0; i < array_size; ++i)
   {
@@ -185,6 +191,7 @@ uPtr_intVec handle_command_line_args(int argc, char **argv, int *array_size, int
         i++;
         for (int j = 0; j < world_size; ++j)
         {
+          // std::cout << recip << std::endl;
           recv = string_to_vec(recip);
         }
       }
@@ -234,13 +241,14 @@ int calculate_partition_size_for_proc(sPtr_intVec recv, int world_rank, int arra
   int elements = array_size / recv->size(); // similar to floor(array_size /recv->size();)
   int position = 0;
   for (auto it = recv->begin(); it != recv->end(); ++it)
-  {
+  { // define position of proc with id "world_rank" in the recv list
     if (*it == world_rank)
       break;
     position++;
   }
+
   int remainder = array_size - elements * recv->size();
-  if (position < remainder)
+  if (position < remainder) // evenly distribute the remainder
     elements++;
 
   return elements;
